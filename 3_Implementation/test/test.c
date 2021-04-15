3da13f94d460c056ae4dbf81e892ac1910a6bed8
Asmita Kajrolkar <80444408+Asmita-Kajrolkar@users.noreply.github.com>
2021-04-15 21:06:56 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -33,29 +33,29 @@ int main()
 
 /* Write all the test functions */ 
 void test_bmi(void) {
-  TEST_ASSERT_EQUAL(10384, bmi(150,40));
-  TEST_ASSERT_EQUAL(103846, bmi(160,50));
-  TEST_ASSERT_EQUAL(1038461, bmi(200,70));
-  TEST_ASSERT_EQUAL(10384616, bmi(250,90));
-  TEST_ASSERT_EQUAL(0, bmi(0,40));
+  TEST_ASSERT_EQUAL(17.8, bmi(40,150));
+  TEST_ASSERT_EQUAL(19.5, bmi(50,160));
+//   TEST_ASSERT_EQUAL(1038461, bmi(200,70));
+//   TEST_ASSERT_EQUAL(10384616, bmi(250,90));
+  TEST_ASSERT_EQUAL(35.7, bmi(70,140));
   /* Dummy fail*/
   //TEST_ASSERT_EQUAL(15000, bmi(2000,50));
 }
 
 void test_bmr(void) {
-  TEST_ASSERT_EQUAL(121306, bmr(150,40,25));
-  TEST_ASSERT_EQUAL(0, bmr(160,50,50));
-  TEST_ASSERT_EQUAL(2593743, bmr(200,70,46));
+  TEST_ASSERT_EQUAL(1173.5, bmr(40,140,25));
+  TEST_ASSERT_EQUAL(1188, bmr(50,160,50));
+//   TEST_ASSERT_EQUAL(2593743, bmr(200,70,46));
   
   /* Dummy fail*/
   //TEST_ASSERT_EQUAL(100,bmr(2000,50,80));
 }
 
 void test_calorie_requirement(void) {
-  TEST_ASSERT_EQUAL(0, calorie_requirement(1560,2));
+  TEST_ASSERT_EQUAL(2145, calorie_requirement(1560,2));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(0, calorie_requirement(2000,6));
+  TEST_ASSERT_EQUAL(3100, calorie_requirement(2000,3));
 }
 
 
